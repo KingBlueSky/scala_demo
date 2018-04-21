85d223313f0b90f510910aebe5adc9cee4a6739b
jbwang0106 <jbwang0106@163.com>
2018-04-21 22:53:54 +0800

    add test class and object

--- scala_program/src/com/sudiyi/scala/classandobejct/ChecksumAccumulator.scala
+++ scala_program/src/com/sudiyi/scala/classandobejct/ChecksumAccumulator.scala
@@ -2,4 +2,39 @@ package com.sudiyi.scala.classandobejct
 
 class ChecksumAccumulator {
   var sum = 0
+  private var avg = 12
+
+  def getAvg(): Int = {
+    return avg
+  }
+
+  def add(b: Byte) = {
+    avg += b
+  }
+
+  def checkAvg(): Int = {
+    return ~(avg & 0xFF) + 1
+  }
+}
+
+import scala.collection.mutable.Map
+
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+
+  def calculate(s: String) = {
+    if (cache.contains(s)) {
+      cache(s)
+    } else {
+      val acc = new ChecksumAccumulator
+      for (c <- s) {
+        acc.add(c.toByte)
+      }
+      val cs = acc.checkAvg()
+      cache += (s -> cs)
+      cs
+    }
+  }
+
+
 }
